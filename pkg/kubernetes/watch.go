a53e9f3f6a8f241f9aa1ba6fb66ee7e9243cb0e2
TheDadi <dario.difazio@axa-winterthur.ch>
2022-11-09 11:41:49 +0100

    revert

---
+++ pkg/kubernetes/watch.go
@@ -0,0 +1,84 @@
+package kubernetes
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/karlkfi/kubexit/pkg/log"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	watchtools "k8s.io/client-go/tools/watch"
+)
+
+type EventHandler func(watch.Event)
+
+// Watch a pod and call the eventHandler (asyncronously) when an
+// event happens. When the supplied context is canceled, watching will stop.
+func WatchPod(ctx context.Context, namespace, podName string, eventHandler EventHandler) error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return fmt.Errorf("failed to configure kubernetes client: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("failed to create kubernetes client: %v", err)
+	}
+
+	// Watch doesn't take name matches, only selectors. So select on name.
+	fieldSelector := fields.OneTermEqualSelector("metadata.name", podName).String()
+
+	// UntilWithSync takes this crazy compound input to List and then Watch.
+	// These functions add our FieldSelector to the requests.
+	// UntilWithSync uses the List to get the current resource version, because
+	// Watch requires an initial resource version to start at, and the resource
+	// version needs to still be in the etcd event history cache.
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (object runtime.Object, e error) {
+			options.FieldSelector = fieldSelector
+			return clientset.CoreV1().Pods(namespace).List(ctx, options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (i watch.Interface, e error) {
+			options.FieldSelector = fieldSelector
+			return clientset.CoreV1().Pods(namespace).Watch(ctx, options)
+		},
+	}
+
+	go func() {
+		ctx, cancel := context.WithCancel(ctx)
+		// cancel the provided context when done, so that caller can block on it
+		defer cancel()
+
+		// watch until deleted
+		_, err := watchtools.UntilWithSync(ctx, lw, &corev1.Pod{}, nil, func(event watch.Event) (bool, error) {
+			if event.Type == watch.Error {
+				log.Warn("Pod Watch: recoverable error:", "name", podName, "object", event.Object)
+				return false, nil
+			}
+
+			eventHandler(event)
+
+			if event.Type == watch.Deleted {
+				log.Info("Pod Watch: pod deleted", "name", podName)
+				return true, nil
+			}
+			return false, nil
+		})
+		// ErrWaitTimeout is returned when the context is canceled.
+		// Since cancellation is the only way we exit, just ignore it.
+		if err != nil && err != wait.ErrWaitTimeout {
+			// TODO: should we do something about this??
+			log.Error(err, "Pod Watch(name): terminal error", "name", podName)
+		}
+		log.Info("Pod Watch(name): done", "name", podName)
+	}()
+
+	return nil
+}
---
+++ pkg/supervisor/supervisor.go
@@ -0,0 +1,189 @@
+package supervisor
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type Supervisor struct {
+	cmd           *exec.Cmd
+	sigCh         chan os.Signal
+	startStopLock sync.Mutex
+	shutdown      bool
+	shutdownTimer *time.Timer
+}
+
+func New(name string, args ...string) *Supervisor {
+	// Don't use CommandContext.
+	// We want the child process to exit on its own so we can return its exit code.
+	// If the child doesn't exit on TERM, then neither should the supervisor.
+	cmd := exec.Command(name, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+	return &Supervisor{
+		cmd:      cmd,
+		shutdown: false,
+	}
+}
+
+func (s *Supervisor) Start() error {
+	s.startStopLock.Lock()
+	defer s.startStopLock.Unlock()
+
+	if s.shutdown {
+		return errors.New("not starting child process: shutdown already started")
+	}
+
+	log.Printf("Starting: %s\n", s)
+	if err := s.cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start child process: %v", err)
+	}
+
+	// Propegate all signals to the child process
+	s.sigCh = make(chan os.Signal, 1)
+	signal.Notify(s.sigCh)
+
+	go func() {
+		for {
+			sig, ok := <-s.sigCh
+			if !ok {
+				return
+			}
+			// log everything but "urgent I/O condition", which gets noisy
+			if sig != syscall.SIGURG {
+				log.Printf("Received signal: %v\n", sig)
+			}
+			// ignore "child exited" signal
+			if sig == syscall.SIGCHLD {
+				continue
+			}
+			err := s.cmd.Process.Signal(sig)
+			if err != nil {
+				log.Printf("Signal propegation failed: %v\n", err)
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (s *Supervisor) Wait() error {
+	defer func() {
+		signal.Reset()
+		if s.sigCh != nil {
+			close(s.sigCh)
+		}
+		if s.shutdownTimer != nil {
+			s.shutdownTimer.Stop()
+		}
+	}()
+	log.Println("Waiting for child process to exit...")
+	return s.cmd.Wait()
+}
+
+func (s *Supervisor) ShutdownNow() error {
+	s.startStopLock.Lock()
+	defer s.startStopLock.Unlock()
+
+	s.shutdown = true
+
+	if !s.isRunning() {
+		log.Println("Skipping ShutdownNow: child process not running")
+		return nil
+	}
+
+	log.Println("Killing child process...")
+	// TODO: Use Process.Kill() instead?
+	// Sending Interrupt on Windows is not implemented.
+	err := s.cmd.Process.Signal(syscall.SIGKILL)
+	if err != nil {
+		return fmt.Errorf("failed to kill child process: %v", err)
+	}
+	return nil
+}
+
+func (s *Supervisor) ShutdownWithTimeout(timeout time.Duration) error {
+	s.startStopLock.Lock()
+	defer s.startStopLock.Unlock()
+
+	s.shutdown = true
+
+	if !s.isRunning() {
+		log.Println("Skipping ShutdownWithTimeout: child process not running")
+		return nil
+	}
+
+	if s.shutdownTimer != nil {
+		return errors.New("shutdown already started")
+	}
+
+	log.Println("Terminating child process...")
+	err := s.cmd.Process.Signal(syscall.SIGTERM)
+	if err != nil {
+		return fmt.Errorf("failed to terminate child process: %v", err)
+	}
+
+	s.shutdownTimer = time.AfterFunc(timeout, func() {
+		log.Printf("Timeout elapsed: %s\n", timeout)
+		err := s.ShutdownNow()
+		if err != nil {
+			// TODO: ignorable?
+			log.Printf("Failed after timeout: %v\n", err)
+		}
+	})
+
+	return nil
+}
+
+func (s *Supervisor) isRunning() bool {
+	// Process set by cmd.Start - means started
+	// https://golang.org/src/os/exec/exec.go?s=11514:11541#L422
+	// ProcessState set by cmd.Wait - means exited
+	// https://golang.org/src/os/exec/exec.go?s=14689:14715#L511
+	return s.cmd.Process != nil && s.cmd.ProcessState == nil
+}
+
+// String joins the command Path and Args and quotes any with spaces
+func (s *Supervisor) String() string {
+	if s.cmd.Path == "" {
+		return ""
+	}
+
+	var buffer bytes.Buffer
+
+	quote := strings.ContainsRune(s.cmd.Path, ' ')
+	if quote {
+		buffer.WriteRune('"')
+	}
+	buffer.WriteString(s.cmd.Path)
+	if quote {
+		buffer.WriteRune('"')
+	}
+
+	if len(s.cmd.Args) > 1 {
+		for _, arg := range s.cmd.Args[1:] {
+			buffer.WriteRune(' ')
+			quote = strings.ContainsRune(arg, ' ')
+			if quote {
+				buffer.WriteRune('"')
+			}
+			buffer.WriteString(arg)
+			if quote {
+				buffer.WriteRune('"')
+			}
+		}
+	}
+
+	return buffer.String()
+}
